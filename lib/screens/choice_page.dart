09398eab70e68a18b426e984a0bc900c83378237
Striver <harshjain2720@gmail.com>
2023-02-24 18:08:34 +0530

    Added welcome,signup,login functionality with animations and constants

---
+++ lib/screens/choice_page.dart
@@ -0,0 +1,12 @@
+// @dart=2.9
+
+import 'package:flutter/material.dart';
+
+class ChoicePage extends StatelessWidget {
+  const ChoicePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("You can now make choice !"));
+  }
+}
